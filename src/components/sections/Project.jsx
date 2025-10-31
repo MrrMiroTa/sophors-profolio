587261a9fbd170838365b3e006e3bee18baddf82
MrPhors <142238273+MrPhors@users.noreply.github.com>
2025-11-01 02:19:34 +0700

    add to link

--- src/components/sections/Project.jsx
+++ src/components/sections/Project.jsx
@@ -100,6 +100,64 @@ function Project() {
               </a>
             </div>
           </div>
+          <div className="p-6 rounded border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition-all">
+            <h3 className="text-xl ffont-bold mb-2">Calculator App</h3>
+            <p className="text-gray-400 mb-4">
+              Developed a calculator app with real-time updates .
+            </p>
+            <div>
+              {["HTML", "CSS", "JavaScript", "React.js"].map(
+                (index, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 cursor-pointer text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                        hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition"
+                  >
+                    {index}
+                  </span>
+                )
+              )}
+              ;
+            </div>
+            <div className="flex justify-between items-center">
+              <a
+                href="https://calculator-app-uzita.vercel.app/"
+                target="_blank"
+                className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+              >
+                View project →
+              </a>
+            </div>
+          </div>
+          <div className="p-6 rounded border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition-all">
+            <h3 className="text-xl ffont-bold mb-2">To Do List</h3>
+            <p className="text-gray-400 mb-4">
+              Developed a to do list app with real-time updates.
+            </p>
+            <div>
+              {["HTML", "CSS", "JavaScript", "React.js"].map(
+                (index, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 cursor-pointer text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20
+                        hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition"
+                  >
+                    {index}
+                  </span>
+                )
+              )}
+              ;
+            </div>
+            <div className="flex justify-between items-center">
+              <a
+                href="https://to-do-list-nine-teal.vercel.app/"
+                target="_blank"
+                className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+              >
+                View project →
+              </a>
+            </div>
+          </div>
         </div>
       </div>
       </RevealOnScroll>
